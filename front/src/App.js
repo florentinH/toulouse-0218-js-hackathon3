10cf7b45687c8283056a200f4293eff1922d5340
Romain Vaur <romainwcs@gmail.com>
2018-06-13 18:32:45 +0200

    modal opérationnelle

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
-import Button from 'react'
+import LoginModal from './loginModal'
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  handleOpen () {
+    this.setState({ open: true })
+  };
+
+  handleClose () {
+    this.setState({ open: false })
+  };
+
+  render () {
     return (
       <div>
-        
+        <LoginModal
+          open={this.state.open}
+          handleOpen={this.handleOpen}
+          handleClose={this.handleClose}
+        />
       </div>
     )
   }
