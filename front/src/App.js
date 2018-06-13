7dd9b5d933a855c3b77f7375c2dff8a9f703af38
Alexandre Morazin <alexandre.morazin@live.fr>
2018-06-13 19:54:07 +0200

    ajout composant upload inactif

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import Button from 'react'
+// import Button from 'react'
+import HomePageContributeur from './HomePageContributeur';
 
 class App extends Component {
   render () {
     return (
       <div>
-        
+        <HomePageContributeur />
       </div>
     )
   }
--- front/src/HomePageContributeur.js
+++ front/src/HomePageContributeur.js
@@ -1,1 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import PropTypes from 'prop-types'
+// import UploadVideo from './UploadVideo'
+
+const styles = {
+  container: {
+    color: 'grey'
+  },
+  paper: {
+    backgroundColor: 'grey'
+  }
+}
+
+class HomePageContributeur extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    const { classes } = this.props
+    return (
+      <div>
+        <Paper className={classes.paper}>
+          <Grid container>
+            <Grid item xs={12} md={8}>
+              
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    )
+  }
+}
+HomePageContributeur.propTypes = {
+  classes: PropTypes.object
+}
+export default withStyles(styles)(HomePageContributeur)
