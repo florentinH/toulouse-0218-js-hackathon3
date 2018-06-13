cc36d556083fb2d652ee6f4f0ffb3ab4babd9d60
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-06-13 17:54:52 +0200

    change App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,23 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 import { PostList } from './Posts'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 
-const App = () => 
-  <Admin title=" Pourquoi n'y a t-il plus de Mamouth ? " dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
-  </Admin>
+// const App = () =>
+
+// export default App
+
+class App extends Component {
+  render () {
+    return (
+      <Admin title=" Pourquoi n'y a t-il plus de Mamouth ? " dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} />
+      </Admin>
+    )
+  }
+}
 
 export default App
