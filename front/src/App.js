043ac5a47b6c866ab28e919be947009c384dde64
Romain Vaur <romainwcs@gmail.com>
2018-06-13 17:27:19 +0200

    commit qui sert à rien

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Button from 'react'
+import Button from '@material-ui/core/Button'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Button>Login</Button>
       </div>
     )
   }
