7c406a689202879faa9bf6668d0409277e41d6f3
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-06-14 14:38:13 +0200

    icon profile et menu

--- front/src/Profile.js
+++ front/src/Profile.js
@@ -1,61 +1,88 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import FormControl from '@material-ui/core/FormControl'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import Work from '@material-ui/icons/Work'
+import classNames from 'classnames'
+import Avatar from '@material-ui/core/Avatar'
+
 
 const styles = theme => ({
   margin: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
-});
+  center: {
+    textAlign: 'center'
+  },
+  row: {
+    display: 'flex'
+  },
+  avatar: {
+    margin: 10
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60
+  }
+})
 
 function Profile (props) {
-  const { classes } = props;
+  const { classes } = props
 
   return (
-    <div>
-      <div className={classes.margin}>
-        <Grid container spacing={8} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
+    <Grid container justify = "center">
+      <div>
+        <div className={classes.row}>
+          <Avatar alt="Remy Sharp" src="/static/images/remy.jpg" className={classes.avatar} />
+          <Avatar
+            alt="Adelle Charles"
+            image="linkedin.png"
+            className={classNames(classes.avatar, classes.bigAvatar)}
+          />
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" label="Nom" />
+            </Grid>
           </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Nom" />
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" label="Prenom" />
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Work />
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" label="Employé" />
+            </Grid>
+          </Grid>
+        </div>
       </div>
-      <div className={classes.margin}>
-        <Grid container spacing={8} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Prenom" />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={classes.margin}>
-        <Grid container spacing={8} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Employé" />
-          </Grid>
-        </Grid>
-      </div>
-    </div>
-  );
+    </Grid>
+  )
 }
 
 Profile.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(Profile);
+export default withStyles(styles)(Profile)
