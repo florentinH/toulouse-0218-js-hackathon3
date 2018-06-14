2a537d973a2d1c7bf86b1abf1f9972dbf594ca4f
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-06-14 10:53:24 +0200

    change in minidrawer

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 import Homepage from './Homepage'
 import Contributor from './Contributor'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import PersistentDrawer from './AppBar'
+import MiniDrawer from './AppBar'
 
 
 class App extends Component {
   render () {
     return (
       <div>
-        <PersistentDrawer />
+        <MiniDrawer />
         <Router>
           <div>
             <Switch>
